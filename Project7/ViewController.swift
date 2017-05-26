b689b09516c6159dd6503733efbcdde17c08ef31
jslusser <jslusser@gmail.com>
2017-05-26 14:17:34 +0800

    Initial commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by James Slusser on 5/26/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
